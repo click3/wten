35066478243cbb4500a9d1d8a8238b4e8859b179
sweetie089 <bnryxx332a@mail.goo.ne.jp>
2011-02-14 00:22:17 +0900

    errorに漏れがあったので追加

--- src/error_normal.cpp
+++ src/error_normal.cpp
@@ -10,7 +10,9 @@ ErrorNormal::ErrorNormal(ERROR_CODE error_code) :
 const wchar_t* ErrorNormal::ToString(void) {
 	switch(code) {
 #define ADD_ERROR(name, message) case ERROR_CODE_##name: return L##message L"\ncode = " L#name
-	ADD_ERROR(UNKNOWN, "不明なエラーが発生しました。");
+	ADD_ERROR(UNKNOWN,			"不明なエラーが発生しました。");
+	ADD_ERROR(INTERNAL_ERROR,		"内部エラーが発生しました。");
+	ADD_ERROR(DXLIB_INTERNAL_ERROR,	"DxLib内部エラーが発生しました。");
 #undef ADD_ERROR
 	}
 	static wchar_t result[2048];
@@ -23,7 +25,7 @@ ERROR_CODE ErrorNormal::GetErrorCode(void) {
 }
 
 void ErrorNormal::Abort(void) {
-	
+	::wprintf(L"Error: %s\n", ToString());
 }
 
 } // errors
