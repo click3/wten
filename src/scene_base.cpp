41fb50621a888349237cffc2605687ce17c51034
sweetie089 <bnryxx332a@mail.goo.ne.jp>
2011-02-13 23:14:36 +0900

    Scenebaseをとりあえず実装した

--- src/scene_base.cpp
+++ src/scene_base.cpp
@@ -4,6 +4,50 @@ namespace wten { namespace scenes {
 
 using namespace utility;
 
+SceneBase::SceneBase() :
+	window_manager(new WindowManager())
+{
+}
+
+// static
+boost::shared_ptr<SceneBase> SceneBase::CreateScene() {
+	SceneBase *ptr = new SceneBase();
+	BOOST_ASSERT(ptr);
+	ptr->this_ptr.reset(ptr);
+	BOOST_ASSERT(ptr->this_ptr);
+	return this_ptr;
+}
+
+boost::optional<boost::shared_ptr<Error> > SceneBase::DoStart(void) {
+	boost::weak_ptr<EventNotifyInterface> ptr;
+	if(!ptr.lock()) {
+		return boost::shared_ptr<Error>(new errors::ErrorNormal(ERROR_CODE_INTERNAL_ERROR));
+	}
+	EventNotify::Regist(this_ptr);
+	return boost::none;
+}
+
+boost::variant<boost::shared_ptr<Error>, boost::optional<boost::shared_ptr<Scene> >, boost::shared_ptr<SceneExit> > SceneBase::DoNextFrame(void) {
+	if(!window_manager) {
+		return boost::shared_ptr<Error>(new errors::ErrorNormal(ERROR_CODE_INTERNAL_ERROR));
+	}
+
+	if(boost::optional<boost::shared_ptr<Error> > error = window_manager->DoEvent()) {
+		return error.get();
+	}
+
+	if(boost::optional<boost::shared_ptr<Error> > error = window_manager->Draw()) {
+		return error.get();
+	}
+
+	return boost::none;
+}
+
+void SceneBase::EventNotifyCallback(boost::shared_ptr<Event> event) {
+	BOOST_ASSERT(window_manager);
+	window_manager->EnqueueEvent(event);
+}
+
 } // scenes
 
 } // wten
--- src/scene_base.h
+++ src/scene_base.h
@@ -1,8 +1,21 @@
 
-namespace wten { namespace scenes {
+namespace wten {
 
-class SceneBase : public Scene {
+class WindowManager;
 
+namespace scenes {
+
+class SceneBase : public Scene, public EventNotifyInterface {
+private:
+	SceneBase();
+public:
+	boost::shared_ptr<SceneBase> CreateScene();
+	boost::optional<boost::shared_ptr<Error> > DoStart(void);
+	boost::variant<boost::shared_ptr<Error>, boost::optional<boost::shared_ptr<Scene> >, boost::shared_ptr<SceneExit> > DoNextFrame(void);
+	void EventNotifyCallback(boost::shared_ptr<Event> event);
+private:
+	boost::shared_ptr<SceneBase> this_ptr;
+	boost::shared_ptr<WindowManager> window_manager;
 };
 
 } // scenes
